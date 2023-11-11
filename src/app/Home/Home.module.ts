f8a7607af096b9af3994c517ca093a8322457388
Carlos Labrin <carlos.labrin15@gmail.com>
2023-11-11 01:19:04 -0300

    respaldo

--- src/app/Home/Home.module.ts
+++ src/app/Home/Home.module.ts
@@ -1,17 +1,23 @@
 import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import { NgModule, createComponent } from "@angular/core";
 
 import { PresentacionComponent } from './Presentacion/presentacion.component';
+import { CreacionesComponent } from './Informacion/creaciones/creaciones.component';
+import { EstudiosComponent } from './Informacion/estudios/estudios.component';
 
 @NgModule({
     declarations: [
-        PresentacionComponent
+        PresentacionComponent,
+        EstudiosComponent,
+        CreacionesComponent
     ],
     exports: [
-        PresentacionComponent
+        PresentacionComponent,
+        EstudiosComponent,
+        CreacionesComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
     ]
 })
 export class HomeModule{
---
+++ src/app/Home/Informacion/creaciones/creaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-creaciones',
+  templateUrl: './creaciones.component.html',
+  styleUrls: ['./creaciones.component.css']
+})
+export class CreacionesComponent {
+
+}
