869126ee2550c9f88eca15d5c2663caa46403b93
Carlos Labrin <carlos.labrin15@gmail.com>
2023-12-23 19:15:21 -0300

    respaldo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { AppComponent } from './app.component';
 import { HomeModule } from './Home/Home.module';
 import { EstudiosComponent } from './Home/Informacion/estudios/estudios.component';
 import { PresentacionComponent } from './Home/Presentacion/presentacion.component';
-// import { MenuComponent } from './Home/Menu-principal/menu.component';
+import { CreacionesComponent } from './Home/Informacion/creaciones/creaciones.component';
+import { MenuComponent } from './Home/Menu-principal/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
     HomeModule,
+    BrowserModule,
     RouterModule.forRoot([
       {path: 'mostrar-estudios', component: EstudiosComponent},
-      {path: 'inicio', component: PresentacionComponent},
-      // {path: 'inicio', component: MenuComponent},
+      {path: 'mostrar-presentacion', component: PresentacionComponent},
+      {path: 'mostrar-creciones', component: CreacionesComponent},
+      {path: 'inicio', component: MenuComponent},
+      {path: '', redirectTo: '/inicio', pathMatch: 'full'},
     ]),
   ],
   providers: [],
